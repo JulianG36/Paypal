49adcf7d2d594fc7d185c570ee7525616f16b5d6
Jacqueline Desautels <jdesautels@uchicago.edu>
2019-10-12 02:36:40 -0500

    allFriends

--- mongodbservices/leaderboardServices.js
+++ mongodbservices/leaderboardServices.js
@@ -12,4 +12,74 @@ function getUsers(_id, callback){
     })
 }
 
+
+function getFirstName(_id, callback){
+    user.find(_id, (err, data) =>{
+        const{firstname, lastname} = data;
+        const foo = {
+            firstname, 
+            lastname,
+        }
+        if(err){
+            console.log(err);
+            callback(err);
+        }else{
+            callback(err, foo);
+        }
+
+    })
+}
+
+function getFirstName(_id, callback){
+    user.find(_id, (err, data) =>{
+        const{finance} = data;
+        const foo = {
+            finance,
+        }
+        if(err){
+            console.log(err);
+            callback(err);
+        }else{
+            callback(err, foo);
+        }
+
+    })
+}
+
+
+function login(email, password, callback) {
+   user.findOne({email},(err, user) => {
+        if (password === user.password) {
+            callback(err, user._id);
+        } else {
+            console.log(err);
+            callback(err);
+        }    
+        callback(err);
+    })
+}
+function signUp(body, callback){
+    const {email} = body;
+    user.findOne({email},(err, user) => {
+        if(user){
+            return "User Already Exists";
+        }else{
+            const newUser = new user();
+            newUser.firstname = body.firstname;
+            newUser.lastname = body.lastname;
+            newUser.email = body.email;
+            newUser.password = body.password;
+            newUser.save(function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("saved");
+                }
+            })
+        }
+    })
+}
+
+
+module.exports.signUp = signUp;
 module.exports.getUsers = getUsers;
